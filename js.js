0a02c49acd28c94ec4a553be6e4d3702648f7c74
“nadirulmi” <ulminadir99@gmail.com>
2023-06-18 22:44:24 -0300

    se agrego otro ciclo y funcion al proyecto

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
-let opcion = Number(prompt("Bienvenido a AlmaKira seleccione lo que está buscando y conocé las ofertas! \n 1. Pantalones \n 2. Remeras \n 3. Sueters \n 4. Salir"));
-switch(opcion){
+function operacion(){
+    let opcion = Number(prompt("Bienvenido a AlmaKira seleccione el número de lo que está buscando y conocé las ofertas! \n 1. Pantalones \n 2. Remeras \n 3. Sueters \n 4. Salir"));
+    while(opcion != 4){
+    switch(opcion){
     case 1: 
         alert("Solo por hoy comprando 2 o más pantalones recibirás un regalo sorpresa de nuestra parte!")
     break;
@@ -12,14 +14,16 @@ switch(opcion){
         alert("Solo por hoy con la compra de un sueter la segunda unidad te queda con un 30% de descuento!")
     break;
 
-    case 4:
-        alert("Muchas gracias por visitar AlmaKira!")
-    break;
-
     default:
         if (opcion !== Number){
-            alert("Debe escribir un número")
+            alert("Marque una opción o el número 4 para salir")
         }
     }
+    opcion = Number(prompt("Bienvenido a AlmaKira seleccione lo que está buscando y conocé las ofertas! \n 1. Pantalones \n 2. Remeras \n 3. Sueters \n 4. Salir"));
+    }
+}
+
+operacion();
+
 
 
